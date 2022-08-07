9d9d5a92a967f0fddd1ba02a6c8ef94de264f517
Ed Chan <60056550+ec8000@users.noreply.github.com>
2022-08-06 23:56:05 -0700

    Update popularity-of-hack.sql

--- popularity-of-hack.sql
+++ popularity-of-hack.sql
@@ -1,8 +1,8 @@
 /*
-Meta/Facebook has developed a new programing language called Hack.
+Meta/Facebook has developed a new programming language called Hack.
 To measure the popularity of Hack they ran a survey with their employees. 
 The survey included data on previous programming familiarity as well as the number of years of experience, 
-age, gender and most importantly satisfaction with Hack. Due to an error location data was not collected, 
+age, gender and most importantly satisfaction with Hack. Due to an error, location data was not collected, 
 but your supervisor demands a report showing average popularity of Hack by office location. 
 Luckily the user IDs of employees completing the surveys were stored.
 Based on the above, find the average popularity of the Hack per office location.
