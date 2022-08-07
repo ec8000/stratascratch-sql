0a9c0a31535250b6054a486dab357e73387b744c
Ed Chan <60056550+ec8000@users.noreply.github.com>
2022-08-06 23:36:35 -0700

    Create find-the-top-ranked-songs-in-2010.sql

---
+++ find-the-top-ranked-songs-in-2010.sql
@@ -0,0 +1,22 @@
+/*
+What were the top 10 ranked songs in 2010?
+Output the rank, group name, and song name but do not show the same song twice.
+Sort the result based on the year_rank in ascending order.
+
+billboard_top_100_year_end
+id:         int
+year:       int
+year_rank:  int
+group_name: varchar
+artist:     varchar
+song_name:  varchar
+*/
+
+SELECT 
+    group_name, 
+    song_name, 
+    year_rank 
+FROM billboard_top_100_year_end
+WHERE year = 2010 AND year_rank <= 10
+GROUP BY group_name, song_name, year_rank 
+ORDER BY year_rank;
