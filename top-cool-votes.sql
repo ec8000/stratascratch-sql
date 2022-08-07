c1d93e755b942d58eeeed19c2c9dd59acc2b71d6
Ed Chan <60056550+ec8000@users.noreply.github.com>
2022-08-06 22:10:09 -0700

    Update and rename 10060-top-cool-votes.sql to top-cool-votes.sql

---
+++ top-cool-votes.sql
@@ -0,0 +1,27 @@
+/* 
+Find the review_text that received the highest number of 'cool' votes.
+Output the business name along with the review text with the highest number of 'cool' votes.
+
+yelp_reviews
+business_name: varchar
+review_id:     varchar
+user_id:       varchar
+stars:         varchar
+review_date:   datetime
+review_text:   varchar
+funny:         int
+useful:        int
+cool:          int
+*/
+
+WITH ranked_reviews AS 
+(
+    SELECT 
+        business_name, 
+        review_text, 
+        DENSE_RANK() OVER (ORDER BY cool DESC) AS rank
+    FROM yelp_reviews
+)
+SELECT business_name, review_text
+FROM ranked_reviews
+WHERE rank = 1;
