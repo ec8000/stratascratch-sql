59b99b712908857d10af3fdd37edfc3e6d92443b
Ed Chan <60056550+ec8000@users.noreply.github.com>
2022-08-06 23:05:16 -0700

    Update average-salaries.sql

--- average-salaries.sql
+++ average-salaries.sql
@@ -23,5 +23,5 @@ SELECT
     department, 
     first_name, 
     salary, 
-    AVG(salary) OVER(PARTITION BY department) AS avg_salary
+    AVG(salary) OVER (PARTITION BY department) AS avg_salary
 FROM employee;
