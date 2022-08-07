3b6f6cbbcb76ad9972fead6da18d8e074430a90d
Ed Chan <60056550+ec8000@users.noreply.github.com>
2022-08-06 22:12:48 -0700

    Update 9680-most-profitable-companies.sql

--- 9680-most-profitable-companies.sql
+++ 9680-most-profitable-companies.sql
@@ -1,19 +1,21 @@
--- Find the 3 most profitable companies in the entire world.
--- Output the result along with the corresponding company name.
--- Sort the result based on profits in descending order.
+/* 
+Find the 3 most profitable companies in the entire world.
+Output the result along with the corresponding company name.
+Sort the result based on profits in descending order.
 
--- forbes_global_2010_2014
--- company:       varchar
--- sector:        varchar
--- industry:      varchar
--- continent:     varchar
--- country:       varchar
--- marketvalue:   float
--- sales:         float
--- profits:       float
--- assets:        float
--- rank:          int
--- forbeswebpage: varchar
+forbes_global_2010_2014
+company:       varchar
+sector:        varchar
+industry:      varchar
+continent:     varchar
+country:       varchar
+marketvalue:   float
+sales:         float
+profits:       float
+assets:        float
+rank:          int
+forbeswebpage: varchar
+*/
 
 WITH ranked_companies AS 
 (
